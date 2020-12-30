01e56994a659f4c643d4918fc79e737dfb0f077e
whitenoise83 <c.ufuk.yildiran@gmail.com>
2020-12-30 18:09:50 +0300

    Add files via upload

---
+++ futureValue.m
@@ -0,0 +1,24 @@
+%Future value calculator
+% N = Number of compounding periods
+% IY = I/Y = Periodic Interest Rate (r)
+% PV = Present Value of the money
+% FV = Future Value of the money
+    
+% ***********************************************************************************************
+% ***********************************************************************************************
+% Example-1: What is the futute value of $ 1000 in two years with a yearly interest rate of %6 ?
+% PV = $ 1000
+% I/Y = 0.06 (yearly interest rate)
+% N = 2 years
+% ***********************************************************************************************
+% ***********************************************************************************************
+% Example-2: What is the futute value of $ 1000 in three months with a yearly interest rate of %6 ?
+% PV = $ 1000
+% I/Y = 0.06/12 = 0.005 (monthly interest rate)
+% N = 3 months
+% ***********************************************************************************************
+% ***********************************************************************************************
+
+function [FV] = futureValue(PV, IY, N)
+	FV = PV * (1 + IY)^N;
+endfunction
